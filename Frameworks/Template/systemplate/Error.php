18e56d8a057233710a72fb1863d9e15e431bb21d
lwp145 <892989183@qq.com>
2018-11-05 10:21:00 +0800

    add files

--- Frameworks/Template/systemplate/Error.php
+++ Frameworks/Template/systemplate/Error.php
@@ -1,18 +1,69 @@
 <?php
 /**
- * Throws PHP5 Error for JMTemplate.
+ * Throws PHP5 exceptions for Template.
  *
  * 系统要求 php5.3+
- *
  */
 
 /**
- *
- * Provides a simple error class for JMTemplate.
- *
+ * Provides a simple error class for Template.
  */
-
-class JMTemplate_Error
+class JMTemplate_Exception extends Exception
 {
+    /**
+     * 错误代码.
+     *
+     * @access public
+     *
+     * @var string
+     */
+    public $code = null;
 
+    /**
+     * 错误信息.
+     *
+     * @access public
+     *
+     * @var array
+     */
+    public $info = array();
+
+    /**
+     * 错误级别.
+     *
+     * @access public
+     *
+     * @var int
+     */
+    public $level = E_USER_ERROR;
+
+    /**
+     * 错误跟踪.
+     *
+     * @access public
+     *
+     * @var array
+     */
+    public $trace = null;
+
+    public function __construct($conf = array())
+    {
+        // set public properties
+        foreach ($conf as $key => $val) {
+            $this->$key = $val;
+        }
+
+        // add a backtrace
+        if ($conf['trace'] === true) {
+            $this->trace = debug_backtrace();
+        }
+    }
+
+    public function __toString()
+    {
+        ob_start();
+        echo get_class($this) . ': ';
+        print_r(get_object_vars($this));
+        return ob_get_clean();
+    }
 }
--- Frameworks/Template/systemplate/Exception.php
+++ Frameworks/Template/systemplate/Exception.php
@@ -1,11 +1,20 @@
 <?php
 /**
- * Throws PHP5 exceptions for JMTemplate.
+ * Throws PHP5 exceptions for Template.
  *
  * 系统要求 php5.3+
+ *
+ * Author: Jiang Lin <linj@jumei.com>
+ *
  */
 
-class JMTemplate_Exception extends Exception
-{
 
+/**
+ *
+ * A simple Template_Exception class.
+ *
+ *
+ */
+
+class Template_Exception extends Exception {
 }
